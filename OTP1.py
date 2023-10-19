fc1b5ed9dd6beec6ff02702a0050917833163140
rahulw1981 <rahulwankhede1981@gmail.com>
2023-10-19 12:04:25 +0530

    OTP3

--- OTP1.py
+++ OTP1.py
@@ -1,6 +1,7 @@
+
+import smtplib
 import random
 from twilio.rest import Client
-import smtplib
 
 account_sid = 'ACf4f7f6195d9a802477e8f551eff6b45d'
 auth_token = '3a8bee5b077481d4a81afa6634c2bb5a'
@@ -8,23 +9,62 @@ client = Client(account_sid, auth_token)
 twilio_num = '+14706169780'
 target = '+919004244603'
 
+def generateOtp(n=6):
+    otp = ""
+    for i in range(n):
+        otp += str(random.randint(0, 9))
+    return otp
 
-def generate_otp():
-    return str(random.randint(100000, 999999))
+def validateMobile(mobile):
+    return len(mobile) == 10 and mobile.isdigit()
 
-def send_otp_via_sms(phone_number, otp):
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body=f"Your OTP is: {otp}",
-        from_=twilio_number,
-        to=phone_number
-    )
-    print("OTP sent successfully.")
 
-def send_otp_via_email(emailid,otp):
-    server=smtplib.SMTP('smtp.gmail.com',587)
+def validateEmailID(receiver):
+    if "@" not in receiver or "." not in receiver:
+        return False
+    return True
+
+def sendOTPOverMobile(target, otp):
+    if (validateMobile(target)):
+        target = "+91" + target
+        message = client.messages.create(
+            body="Your OTP is " + otp,
+            from_=twilio_num,
+            to=target
+        )
+
+        print(message.body)
+        print("Check Mobile Number", target)
+    else:
+        print("Enter valid mobile number!!")
+
+def sendOTPOverEmail(receiver, otp):
+    body = "Your OTP is " + otp
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender, password)
+    server.sendmail(sender, receiver, body)
+
+    print("Mail sent - OTP: ", otp)
 
 
 if __name__ == "__main__":
-    otp = generate_otp()
-    send_otp_via_sms(target_number, otp)
+
+    print("Send OTP via email\n")
+    sender = "rahulwankhede1981@gmail.com"
+    password = "charkwwceuseemjd"
+    receiver = input("Enter mail: ")
+    otp = generateOtp(6)
+    if (validateEmailID(receiver)):
+        sendOTPOverEmail(receiver, otp)
+    else:
+        print("Enter your valid mail!!")
+
+    send_twilio = input("\nWant to send OTP via SMS: ")
+    if send_twilio.lower() == "yes":
+        target = input("Enter mobile: ")
+        sendOTPOverMobile(target, otp)
+    else:
+        print("\nProgram Ended\n")
+
